4db76a43bd811ac829602b52e4c83138adb13b03
TiagoCastilhos <tiagocastilhos66@gmail.com>
2022-04-09 14:50:50 -0300

    finished exercise 1

--- Exercise 1/main.go
+++ Exercise 1/main.go
@@ -6,12 +6,14 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
-	flag.Parse()
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 
+	flag.Parse()
 	file, err := os.Open(*csvFilename)
 
 	if err != nil {
@@ -24,17 +26,30 @@ func main() {
 	if err != nil {
 		exit(fmt.Sprintf("Failed to parse the provided CSV file: %s", *csvFilename))
 	}
-
 	problems := parseLines(lines)
 
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	correct := 0
-	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s \n", i+1, p.question)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
 
-		if answer == p.answer {
-			correct++
+problemLoop:
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = ", i+1, p.question)
+		answerCh := make(chan string)
+
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println()
+			break problemLoop
+		case answer := <-answerCh:
+			if answer == p.answer {
+				correct++
+			}
 		}
 	}
 
